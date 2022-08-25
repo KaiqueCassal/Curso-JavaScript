7f3c9aa7f64fdc13f28e627b17a65f29311d3259
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-25 01:40:42 -0300

    aula007
    Lista.

--- aula/007-lista/01.js
+++ aula/007-lista/01.js
@@ -10,9 +10,16 @@ console.log(num)
 console.log(num.length)
 console.log(num.sort())
 
-pos = 1
-
+/*
+pos = 0
 while(pos <= num.length){
     console.log(num[pos])
     pos++
 }
+*/
+
+for(let pos in num.sort()){
+    console.log(num[pos])
+}
+
+console.log(num.indexOf(70))
